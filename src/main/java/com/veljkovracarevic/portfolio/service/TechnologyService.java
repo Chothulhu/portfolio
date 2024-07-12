b00e2d40f8123d6d8ddf2e8526d1779a40cd099a
Veljko Vračarević <vracarevicveljko@gmail.com>
2024-07-12 14:50:24 +0200

    Project service implemented

--- src/main/java/com/veljkovracarevic/portfolio/service/TechnologyService.java
+++ src/main/java/com/veljkovracarevic/portfolio/service/TechnologyService.java
@@ -2,6 +2,13 @@ package com.veljkovracarevic.portfolio.service;
 
 import com.veljkovracarevic.portfolio.dto.TechnologyDto;
 
+import java.util.List;
+
 public interface TechnologyService {
-    TechnologyDto createTechnology (TechnologyDto technologyDto);
+    TechnologyDto createTechnology (TechnologyDto techDto);
+
+    List<TechnologyDto> getAllTechnologies();
+    TechnologyDto getTechnologyById(int id);
+    TechnologyDto updateTechnology(TechnologyDto techDto, int id);
+    void deleteTechnology(int id);
 }
